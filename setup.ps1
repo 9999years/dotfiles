e14618a6ea61bb1c6989a1baf29032d126d5f9d4
Rebecca Turner <637275@gmail.com>
2018-11-06 10:29:18 -0500

    pythonrc, improved setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -2,7 +2,7 @@
 .PARAMETER Name
 The name of a file in this directory, like ".gitconfig" or ".minttyrc"
 #>
-[CmdletBinding()]
+[CmdletBinding(SupportsShouldProcess = $True)]
 Param(
 	[Parameter(
 		ValueFromPipeline=$True
@@ -15,37 +15,23 @@ Param(
 		"pip.conf",
 		"_curlrc",
 		"youtube-dl.conf",
+		".pythonrc",
+		".ptpython/config.py",
 		"AppData/Roaming/ConEmu.xml"),
-	[ValidateSet("Force", "Quit", "Skip", "Ask")]
-	[String]$Overwrite="Ask"
+	[Switch]$Force
 )
 
 Begin {
 	"Linking files"
-
-	function PromptForChoice {
-		[CmdletBinding()]
-		Param(
-			[String]$Message,
-			[String]$Question,
-			[String[]]$Choices=("&Yes", "&No"),
-			[Int]$DefaultChoice=1
-		)
-
-		$choiceList = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
-		$Choices | %{
-			$choiceList.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList $_))
-		}
-
-		return $Host.UI.PromptForChoice($Message, $Question, $choiceList, $DefaultChoice)
+	if ($ConfirmPreference -eq 'Low') {
+		$YesToAll = $False
+	} else {
+		$YesToAll = $True
 	}
-
-	$choice = Switch($Overwrite) {
-		"Force" { 0 }
-		"Quit"  { 1 }
-		"Skip"  { 2 }
-		Default { -1 }
+	if ($Force) {
+		$YesToAll = $True
 	}
+	$NoToAll = $False
 }
 
 Process {
@@ -54,43 +40,18 @@ Process {
 		$To = Join-Path $PSScriptRoot $Name
 		"Linking from $From to $To" | Write-Verbose
 
-		$skip = $False
-
 		If(Test-Path $From) {
 			# file already exists
-			If($Overwrite -eq "Ask") {
-				# ask the user
-				$choice = PromptForChoice `
-					-Message "$From already exists" `
-					-Question "Write anyways?" `
-					-Choices ("&Force", "&Quit", "&Skip") `
-					-DefaultChoice 1
-			}
 
-			Switch($choice) {
-				0 { Remove-Item $From -Force }
-				# quit; magic number means "file already exists"
-				1 { Throw [System.IO.IOException]::new(
-					"$From already exists!", 0x80070050) }
-				Default {
-					"Skipping"
-					# Continue keyword doesnt work here
-					# for. Reasons
-					$skip = $True
-				}
+			If($PSCmdlet.ShouldContinue("Overwrite $From with a link to $To?",
+					"Confirm overwrite", [ref]$YesToAll, [ref]$NoToAll)) {
+				Remove-Item $From -Confirm:$False
 			}
-		}
-
-		If($skip) {
-			Continue
 		}
 
 		#mklink
-		New-Item -Path $From -Value $To -ItemType SymbolicLink | `
-		%{ $_.Name }
+		New-Item -Path $From -Value $To -ItemType SymbolicLink -Confirm:$False |
+			Out-Null
+		$Name
 	}
-}
-
-End {
-	"To restore local group policy run './gp/restore.ps1'; this will overwrite all current group policy! Use https://github.com/dlwyatt/PolicyFileEditor/blob/master/Commands.ps1 instead?"
 }
