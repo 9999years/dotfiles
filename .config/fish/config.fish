583b0aafe252cd43170aa31e78243e05cbbec818
Rebecca Turner <637275@gmail.com>
2019-02-14 13:05:54 -0500

    fish ldflags, cflags, etc.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,10 +3,10 @@ set -gx LS_OPTIONS "--color=auto"
 set -gx EDITOR vim
 
 set -gx PATH $HOME/.local/bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin
-set -gx LD_LIBRARY_PATH "$HOME/.local/lib"
-set -gx LD_RUN_PATH "$HOME/.local/lib"
-set -gx LDFLAGS "-L$HOME/.local/lib"
-set -gx CFLAGS "-I$HOME/.local/include"
+set -gx LD_LIBRARY_PATH "$HOME/.local/lib:$HOME/.local/lib64:/usr/local/lib:/usr/local/lib64:/lib64:/usr/lib64"
+set -gx LD_RUN_PATH "$HOME/.local/lib:$HOME/.local/lib64:/usr/local/lib:/usr/local/lib64:/lib64:/usr/lib64"
+set -gx LDFLAGS "-L$HOME/.local/lib -L$HOME/.local/lib64 -L/usr/local/lib -L/usr/local/lib64 -L/lib64 -L/usr/lib64"
+set -gx CFLAGS "-I$HOME/.local/include -I/usr/local/include"
 set -gx MANPATH "$HOME/.local/share/man:$HOME/.local/man:/usr/share/man"
 
 abbr ll 'ls -la'
