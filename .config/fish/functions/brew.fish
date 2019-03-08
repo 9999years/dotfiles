da359e4961db423350b1cdf6a11b5fc373fc154d
Rebecca Turner <637275@gmail.com>
2019-03-08 15:44:24 -0500

    brew command; more mac stuff

---
+++ .config/fish/functions/brew.fish
@@ -0,0 +1,8 @@
+# Defined in /var/folders/zz/zyxvpxvq6csfxvn_n0001mw8000d72/T//fish.t5QJav/brew.fish @ line 2
+function brew
+	set BREW_PATH --path /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /bin /sbin
+	set BREW_HOME /var/homebrew
+	pushd "$BREW_HOME"
+	env HOME="$BREW_HOME" PATH="$BREW_PATH" sudo -u homebrew /usr/local/bin/brew $argv
+	popd
+end
--- .config/fish/functions/tarwget.fish
+++ .config/fish/functions/tarwget.fish
@@ -5,7 +5,11 @@ function tarwget --argument url
 		#| sed -En 's/^Content-Length: ([0-9]+)\r/\1/p' \
 		#| numfmt --to=iec)
 	echo "Downloading to $file"
-	wget -q $url -O $file
+	if which wget
+		wget -q $url -O $file
+	else
+		curl -L -o $file $url
+	end
 	and echo "Extracting archive"
 	and tar -xf $file
 	and rm $file
