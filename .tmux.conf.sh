f2c17a777c41219dbd68fac833f7cdb086fdf124
Rebecca Turner <637275@gmail.com>
2019-03-12 14:22:13 -0400

    tmux system copy on os x

--- .tmux.conf.sh
+++ .tmux.conf.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-if which fish
+if which fish > /dev/null
 then
 	tmux set -g default-shell "$(which fish)"
+fi
+
+if [[ `uname` == "Darwin" ]]
+then
+	tmux bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel "pbcopy"
 fi
