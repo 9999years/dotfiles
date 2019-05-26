3dfdee0dc16d8efef5b308c4696dda53eb94d234
Rebecca Turner <637275@gmail.com>
2019-05-25 23:24:28 -0400

    Fisher, TPM

--- .config/fish/functions/brew.fish
+++ .config/fish/functions/brew.fish
@@ -1,8 +1,13 @@
 # Defined in /var/folders/zz/zyxvpxvq6csfxvn_n0001mw8000d72/T//fish.Ciqqs2/brew.fish @ line 2
 function brew
-	set BREW_PATH --path /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /bin /sbin
-	set BREW_HOME /var/homebrew
-	pushd "$BREW_HOME"
-	env HOME="$BREW_HOME" PATH="$BREW_PATH" sudo -u homebrew /usr/local/bin/brew $argv
-	popd
+	switch (hostname -f)
+		case "*.brandeis.edu"
+			set BREW_PATH --path /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /bin /sbin
+			set BREW_HOME /var/homebrew
+			pushd "$BREW_HOME"
+			env HOME="$BREW_HOME" PATH="$BREW_PATH" sudo -u homebrew /usr/local/bin/brew $argv
+			popd
+		case "*"
+			env brew $argv
+	end
 end
