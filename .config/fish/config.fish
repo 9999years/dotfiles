bd1bcb41f6f2cf390a9c4d924e9f9ae30e08f229
Rebecca Turner <637275@gmail.com>
2019-05-25 23:24:28 -0400

    Fisher, TPM

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,6 +21,16 @@ if not _has_version 3.0.0
 	exit
 end
 
+set -g fisher_path $HOME/.config/fisher/
+mkdir -p "$fisher_path"
+
+set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
+set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
+
+for file in $fisher_path/conf.d/*.fish
+    builtin source $file 2> /dev/null
+end
+
 function is_darwin
 	test (uname) = "Darwin"
 end
