e91a3004d307588ec31e4a9f970ddfbc62c68947
Rebecca Turner <637275@gmail.com>
2019-06-12 21:36:44 -0700

    Fish abbrs
    fish:
    - c1 through c6 abbreviations
    - gr = cd to git root
    - ii = explorer.exe on windows
    
    tmux:
    - better support for copying on windows

--- .tmux.conf.sh
+++ .tmux.conf.sh
@@ -5,7 +5,24 @@ then
 	tmux set -g default-shell "$(which fish)"
 fi
 
-if [[ `uname` == "Darwin" ]]
+uname="$(uname)"
+
+bind_copy=(bind-key -T copy-mode-vi MouseDragEnd1Pane)
+
+function tmux_bind_copy {
+	tmux "${bind_copy[@]}" send-keys -X copy-pipe-and-cancel "$@"
+}
+
+function is_wsl {
+	[[ ! -z "$WSL_DISTRO_NAME" ]]
+}
+
+if [[ "$uname" == "Darwin" ]]
 then
-	tmux bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel "pbcopy"
+	tmux_bind_copy pbcopy
+fi
+
+if is_wsl
+then
+	tmux_bind_copy /mnt/c/Windows/System32/clip.exe
 fi
