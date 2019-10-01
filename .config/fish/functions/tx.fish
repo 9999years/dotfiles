389d0a2fed03c69066c189b5d1fe7c44bb92ed7e
Rebecca Turner <rbt@sent.as>
2019-10-01 14:26:14 -0400

    fish tx

--- .config/fish/functions/tx.fish
+++ .config/fish/functions/tx.fish
@@ -1,10 +1,11 @@
-# Defined in /tmp/fish.0BD3bo/tx.fish @ line 2
+# Defined in /tmp/fish.4pPQDK/tx.fish @ line 2
 function tx --argument glob
 	set -l processed_glob ./*$glob*.tex
     if test -z "$glob"
+        # No glob given in arguments; use fzf
         set processed_glob (fd .tex | fzf)
-        echo $processed_glob
     else
+        # Use partial glob from arguments
         switch $glob
             case "*.tex"
                 set processed_glob ./*$glob
@@ -12,10 +13,15 @@ function tx --argument glob
                 set processed_glob $glob*.tex
         end
     end
-    if test -z $processed_glob
+
+    if test -n "$glob"
+        echo -e "\e[1m\e[31mSearch pattern cannot be empty.\e[0m"
+        return 1
+    else if test -z $processed_glob
         echo -e "\e[1m\e[31mNo matches found for glob '$glob'.\e[0m"
+        return 1
     else
-        echo -e "\e[1m\e[4mlatexmk" $processed_glob "\e[0m"
+        echo -e "\e[1m\e[4mlatexmk" $processed_glob"\e[0m"
         latexmk $processed_glob
     end
 end
