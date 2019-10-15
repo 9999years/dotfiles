07c6bdac2aef1937bf54066d3e9a945e24da3fa1
Rebecca Turner <rbt@sent.as>
2019-10-15 14:34:18 -0400

    git command fallback to git

--- .config/fish/functions/git.fish
+++ .config/fish/functions/git.fish
@@ -1,3 +1,8 @@
+# Defined in /var/folders/zz/zyxvpxvq6csfxvn_n0001mw8000d72/T//fish.Sh4Ycc/git.fish @ line 2
 function git
-	hub $argv
+	if command -v hub
+        hub $argv
+    else
+        command git $argv
+    end
 end
