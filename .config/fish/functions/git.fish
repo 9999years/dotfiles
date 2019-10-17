44c977f21de55a108af21c54e56d8a3198e49f7b
Rebecca Turner <rbt@sent.as>
2019-10-17 16:31:55 -0400

    hub: don't echo hub binary location

--- .config/fish/functions/git.fish
+++ .config/fish/functions/git.fish
@@ -1,6 +1,6 @@
-# Defined in /var/folders/zz/zyxvpxvq6csfxvn_n0001mw8000d72/T//fish.Sh4Ycc/git.fish @ line 2
+# Defined in /var/folders/zz/zyxvpxvq6csfxvn_n0001mw8000d72/T//fish.h9wbqa/git.fish @ line 2
 function git
-	if command -v hub
+	if command -v hub > /dev/null
         hub $argv
     else
         command git $argv
