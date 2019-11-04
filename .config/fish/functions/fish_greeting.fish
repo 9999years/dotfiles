7f7db0ffec26ad9ff4bbdedb1653d9732f1c753f
Rebecca Turner <rbt@sent.as>
2019-11-03 21:08:16 -0500

    better fish_greeting

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,4 +1,9 @@
-# Defined in /tmp/fish.3sakXD/fish_greeting.fish @ line 2
+# Defined in /tmp/fish.iF4bpi/fish_greeting.fish @ line 2
 function fish_greeting
-	fish_logo
+	set_color --bold blue
+    and echo "                       Hello miss!"
+    and set_color normal
+    and set_color green
+    and uptime
+    and set_color normal
 end
