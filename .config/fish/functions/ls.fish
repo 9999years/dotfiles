aa09a3e43d01d607d1038926ab5134dd41f35c4c
Rebecca Turner <rbt@sent.as>
2020-01-21 21:42:46 -0500

    add kglobalshortcutsrc

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.ITfxjb/ls.fish @ line 2
-function ls --description 'List contents of directory'
+# Defined in - @ line 2
+function ls --description 'Lists files and directories'
 	command exa $argv
 end
