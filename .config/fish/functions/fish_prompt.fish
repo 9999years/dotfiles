d12512049eaee8199f8c6c88da586cd3cddd1218
Rebecca Turner <rbt@sent.as>
2020-08-09 12:56:30 -0400

    update prompt, nixos-full-boot-fix

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,14 +1,12 @@
-# Defined in - @ line 2
+# Defined in /tmp/fish.TFyyWX/fish_prompt.fish @ line 2
 function fish_prompt --description 'Write out the prompt'
 	set -l color_cwd
     set -l suffix
     set -l nix_shell_info (
-  if test -n "$IN_NIX_SHELL"
-    echo -n -s (set_color cyan) "<nix-shell>" (set_color normal)
-  else
-    echo -n "$USER"
-  end
-)
+        if test -n "$IN_NIX_SHELL"
+            echo -n -s (set_color cyan) "<nix-shell> " (set_color normal)
+        end
+    )
     switch "$USER"
         case root toor
             if set -q fish_color_cwd_root
@@ -19,8 +17,8 @@ function fish_prompt --description 'Write out the prompt'
             set suffix '#'
         case '*'
             set color_cwd $fish_color_cwd
-            set suffix '>'
+            set suffix ';'
     end
 
-    echo -n -s "$nix_shell_info" @ (prompt_hostname) ' ' (set_color $color_cwd) (prompt_pwd) (set_color normal) "$suffix "
+    echo -n -s "$nix_shell_info" (set_color $color_cwd) (prompt_pwd) (set_color normal) " $suffix "
 end
--- .config/fish/functions/nixos-full-boot-fix.fish
+++ .config/fish/functions/nixos-full-boot-fix.fish
@@ -1,7 +1,17 @@
-# Defined in /tmp/fish.3u2CG6/nixos-full-boot-fix.fish @ line 2
+# Defined in /tmp/fish.L8naA7/nixos-full-boot-fix.fish @ line 2
 function nixos-full-boot-fix
-	sudo nix-env --delete-generations old
+	echo -s (set_color --bold brcyan) "Removing old generations" (set_color normal)
+    echo -s (set_color --underline ) "sudo nix-env --delete-generations old" \
+        (set_color normal) (set_color --dim) \
+        "  # our profile, all non-current generations" (set_color normal)
+    sudo nix-env --delete-generations old
+    echo -s (set_color --underline ) "sudo nix-env -p /nix/var/nix/profiles/system --delete-generations +2" \
+        (set_color normal) (set_color --dim) \
+        "  # system profile, all but the last 2 generations" (set_color normal)
     sudo nix-env -p /nix/var/nix/profiles/system --delete-generations +2
-    sudo nix-store --gc
-    sudo nix-collect-garbage -d
+    echo -s (set_color --bold brcyan) \
+        "Finished removing old generations; consider additionally cleaning the nix store:" \
+        (set_color normal)
+    echo "    " (set_color --bold green) "sudo nix-store --gc" (set_color normal)
+    echo "    " (set_color --bold green) "sudo nix-collect-garbage -d" (set_color normal)
 end
