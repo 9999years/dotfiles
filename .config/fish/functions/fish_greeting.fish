710f381551e55b8e5cf4f0ed3105335b51267ce1
Rebecca Turner <rbt@sent.as>
2020-08-30 15:50:00 -0400

    add puppy to fish_greeting

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,8 +1,11 @@
-# Defined in /tmp/fish.w25efJ/fish_greeting.fish @ line 2
+# Defined in /tmp/fish.Ob4vgX/fish_greeting.fish @ line 2
 function fish_greeting
     if test -z "$IN_NIX_SHELL"
         echo -s (set_color --bold blue) "    Hello miss! " (set_color normal) \
                    (set_color green) (uptime) (set_color normal)
+        if command -vq puppy
+            echo -s (set_color magenta) (puppy) (set_color normal)
+        end
     else
         echo -s -n (set_color --bold blue) "🐟  " (set_color normal)
     end
