e6d8a66f50491053d540c68b4febe7ac97c044bf
Rebecca Turner <rbt@sent.as>
2022-03-12 14:58:31 -0500

    Update tmux conf for macos/linux

--- tmux.conf.sh
+++ tmux.conf.sh
@@ -6,20 +6,14 @@ then
 	tmux set -g default-shell "$(command -v fish)"
 fi
 
-uname="$(uname)"
-
 bind_copy=(bind-key -T copy-mode-vi MouseDragEnd1Pane)
 
 function tmux_bind_copy {
 	tmux "${bind_copy[@]}" send-keys -X copy-pipe "$@"
 }
 
-function is_wsl {
-	[[ -n "$WSL_DISTRO_NAME" ]]
-}
-
-if [[ "$uname" == "Darwin" ]]; then
+if [[ "$(uname)" == "Darwin" ]]; then
 	tmux_bind_copy pbcopy
-elif is_wsl; then
+elif [[ -n "$WSL_DISTRO_NAME" ]]; then
 	tmux_bind_copy /mnt/c/Windows/System32/clip.exe
 fi
