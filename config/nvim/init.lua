004c933b609a25f45d8d91ee599a3a1c0be3cecc
Rebecca Turner <rbt@sent.as>
2022-04-13 13:04:43 -0400

    telescope bindings

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -66,6 +66,7 @@ require('packer').startup(function(use)
         nnoremap <leader>f :<C-u>Telescope find_files<CR>
         nnoremap <leader>b :<C-u>Telescope buffers<CR>
         nnoremap <leader>g :<C-u>Telescope live_grep<CR>
+        nnoremap <leader>h :<C-u>Telescope oldfiles<CR>
       ]]
     end,
     requires = { "nvim-lua/popup.nvim", "nvim-lua/plenary.nvim" },
@@ -100,7 +101,7 @@ require('packer').startup(function(use)
         buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
         buf_set_keymap("n", "<space>D",  "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
         buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-        buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+        buf_set_keymap("n", "<space>ca", ":<C-u>Telescope lsp_code_actions<CR>", opts)
         buf_set_keymap("n", "gr",        "<cmd>lua vim.lsp.buf.references()<CR>", opts)
         buf_set_keymap("n", "<space>e",  "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
         buf_set_keymap("n", "[d",        "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
