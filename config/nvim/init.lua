a248a4972d43d86deff4d76448c18051bd10042c
Rebecca Turner <rbt@sent.as>
2022-06-07 09:51:29 -0400

    Add gitsigns mappings

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -145,7 +145,56 @@ require("packer").startup(function(use)
   use {
     "lewis6991/gitsigns.nvim", -- Git gutter
     config = function()
-      require("gitsigns").setup()
+      require("gitsigns").setup {
+        on_attach = function(bufnr)
+          local gs = require("gitsigns")
+          local batteries = require("batteries")
+
+          batteries.map {
+            buffer = bufnr,
+
+            -- Navigation
+            {
+              "]c",
+              function()
+                if vim.wo.diff then
+                  return "]c"
+                end
+                vim.schedule(function() gs.next_hunk() end)
+                return "<Ignore>"
+              end,
+              "Next diff hunk",
+              expr = true
+            },
+            {
+              "[c",
+              function()
+                if vim.wo.diff then
+                  return "[c"
+                end
+                vim.schedule(function() gs.prev_hunk() end)
+                return "<Ignore>"
+              end,
+              "Prev diff hunk",
+              expr = true
+            },
+
+            -- Text object
+            { "ih", "<Cmd>Gitsigns select_hunk<CR>", "Hunk", mode = {"o", "x"} },
+
+            -- Actions
+            { prefix = "<Leader>h", name = "+hunk" },
+            { "<Leader>hs", "<Cmd>Gitsigns stage_hunk<CR>", "Stage hunk", mode = {"n", "v"} },
+            { "<Leader>hr", "<Cmd>Gitsigns reset_hunk<CR>", "Reset (unstage) hunk", mode = {"n", "v"} },
+            { "<Leader>hS", gs.stage_buffer, "Stage buffer" },
+            { "<Leader>hu", gs.undo_stage_hunk, "Undo stage hunk" },
+            { "<Leader>hR", gs.reset_buffer, "Reset buffer" },
+            { "<Leader>hp", gs.preview_hunk, "Preview hunk" },
+            { "<Leader>hb", function() gs.blame_line { full = true } end, "Blame line" },
+            { "<Leader>hD", function() gs.diffthis("~") end, "Diff" },
+          }
+        end
+      }
     end,
   }
 
