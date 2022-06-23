57d26fe8c9579277c4b1eec13e320d1f1e27e31b
Rebecca Turner <rbt@sent.as>
2022-06-23 19:36:36 -0400

    Add fish_greeting to clear

--- config/fish/functions/clear.fish
+++ config/fish/functions/clear.fish
@@ -1,8 +1,9 @@
 # Defined in /tmp/fish.FeQd6L/clear.fish @ line 2
 function clear
     command clear
+    fish_greeting
     set -ge __prompt_last_dir
-    # Then, if we're running tmux, clear the backscroll buffer too.
+    # If we're running tmux, clear the backscroll buffer too.
     # This lets us scroll all the way up without seeing the stuff we
     # thought we just cleared.
     if test -n "$TMUX"
