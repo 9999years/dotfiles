27c59dc369692e4b474082536c62fc7bdb5504e2
Rebecca Turner <rbt@sent.as>
2022-06-24 16:42:24 -0400

    Support `nix shell` in `_tide_item_nix`

--- config/fish/functions/_tide_item_nix.fish
+++ config/fish/functions/_tide_item_nix.fish
@@ -1,4 +1,4 @@
 function _tide_item_nix
     set -q tide_nix_icon; or set -l tide_nix_icon '❄'
-    set -q IN_NIX_SHELL && _tide_print_item nix $tide_nix_icon
+    set -q IN_NIX_SHELL || set -q IN_NIX_RUN && _tide_print_item nix $tide_nix_icon
 end
