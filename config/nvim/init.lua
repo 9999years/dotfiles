994c7a284e66c83a06f3f7ae31543d93d73996be
Rebecca Turner <rbt@sent.as>
2022-07-05 11:09:55 -0400

    Add dash.nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -164,6 +164,14 @@ require("packer").startup(function(use)
     end,
   }
 
+  -- Only add `dash.nvim` if `Dash.app` is installed.
+  if vim.fn.empty(vim.fn.glob("/Applications/Dash.app/")) == 0 then
+    use {
+      "mrjones2014/dash.nvim",
+      run = "make install",
+    }
+  end
+
   -- LSP configuration
   use("neovim/nvim-lspconfig")
   -- Autoformat on save:
