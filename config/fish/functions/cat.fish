19d4ef54cad5ce112ab8906d187163692533245b
Rebecca Turner <rbt@sent.as>
2022-10-30 23:09:51 -0400

    `cat` shouldn't call `exa`

--- config/fish/functions/cat.fish
+++ config/fish/functions/cat.fish
@@ -1,16 +1,8 @@
 # Defined in /tmp/fish.MXQeQ6/cat.fish @ line 2
 function cat
-    if test -d $argv[-1]
-        if command -v exa >/dev/null
-            command exa -la $argv
-        else
-            command ls -la $argv
-        end
+    if command -v bat >/dev/null
+        command bat $argv
     else
-        if command -v bat >/dev/null
-            command bat $argv
-        else
-            command cat $argv
-        end
+        command cat $argv
     end
 end
