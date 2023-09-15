16e49c403b380a6af300048d796ab38257fa904a
Rebecca Turner <rbt@sent.as>
2023-09-15 12:05:42 -0700

    Auto install treesitter grammars + incremental_selection

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -50,12 +50,22 @@ require("packer").startup(function(use)
     config = function()
       -- See: https://github.com/nvim-treesitter/nvim-treesitter#available-modules
       require("nvim-treesitter.configs").setup {
+        auto_install = true,
         highlight = {
           enable = true,
           additional_vim_regex_highlighting = false,
         },
         indent = {
           enable = true,
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "gnn", -- set to `false` to disable one of the mappings
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+          },
         },
       }
 
