de84ff2019e1a5b8f7b2ec021c6e70da34afba3d
Rebecca Turner <rbt@sent.as>
2023-10-01 13:34:58 -0700

    tide config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -28,24 +28,6 @@ abbr --add --global replay bass
 
 # Prompt.
 fundle plugin IlanCosman/tide
-# Nix logo colors: 8BB6DE 5873BA
-set -g tide_nix_color 8BB6DE
-# Something about Fish/Alacritty gets weird about printing these nonstandardly
-# double-wide characters unless the background color is unset (it cuts off the
-# right half of the character), so we use the special `normal` keyword for the
-# background color. (See `man set_color`.)
-set -g tide_nix_bg_color normal
-set -g tide_nix_icon ' ' # ❆❄
-if ! contains nix $tide_left_prompt_items
-    set --prepend tide_left_prompt_items nix
-end
-# I don't need fancy shapes, and my `normal` bg_color hack above plays poorly
-# with them anyways.
-set -g tide_left_prompt_prefix ''
-set -g tide_left_prompt_suffix ''
-set -g tide_right_prompt_prefix ''
-set -g tide_right_prompt_suffix ''
-set -g tide_character_icon '$'
 
 # Directory jumping.
 fundle plugin jethrokuan/z
---
+++ config/fish/functions/tide-reconfigure.fish
@@ -0,0 +1,30 @@
+function tide-reconfigure
+    tide configure --auto \
+        --style=Rainbow \
+        --prompt_colors='True color' \
+        --show_time='12-hour format' \
+        --rainbow_prompt_separators=Vertical \
+        --powerline_prompt_heads=Sharp \
+        --powerline_prompt_tails=Flat \
+        --powerline_prompt_style='Two lines, character' \
+        --prompt_connection=Disconnected \
+        --powerline_right_prompt_frame=No \
+        --prompt_spacing=Compact \
+        --icons='Many icons' \
+        --transient=Yes
+
+    # Nix logo colors: 8BB6DE 5873BA
+    set -U tide_nix_color 8BB6DE
+    set -U tide_nix_bg_color black
+    set -U tide_nix_icon ' ' # ❆❄
+    if ! contains nix $tide_left_prompt_items
+        set --prepend tide_left_prompt_items nix
+    end
+    # I don't need fancy shapes, and my `normal` bg_color hack above plays poorly
+    # with them anyways.
+    set -U tide_left_prompt_prefix ''
+    set -U tide_left_prompt_suffix ''
+    set -U tide_right_prompt_prefix ''
+    set -U tide_right_prompt_suffix ''
+    set -U tide_character_icon '$'
+end
