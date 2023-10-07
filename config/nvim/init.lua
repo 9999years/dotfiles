b903db48f1ff3b5cb75ada7e35187cebedff5c40
Rebecca Turner <rbt@sent.as>
2023-10-07 15:35:04 -0700

    Remove custom markdown parser
    It's merged now

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -68,18 +68,6 @@ require("lazy").setup {
         },
       }
 
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      parser_config.markdown = {
-        install_info = {
-          url = "https://github.com/MDeiml/tree-sitter-markdown.git",
-          files = { "src/parser.c" },
-          branch = "main",
-          generate_requires_npm = false,
-          requires_generate_from_grammar = false,
-        },
-        filetype = "md",
-      }
-
       -- https://neovim.discourse.group/t/git-diff-highlighting-are-not-working-anymore-in-gitcommit-filetype/3547/5
       vim.cmd([[
         highlight def link @text.diff.add DiffAdded
