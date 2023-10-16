136b1e11e0dfae58c8675db803427998dc5853d2
Rebecca Turner <rbt@sent.as>
2023-10-16 12:36:19 -0700

    Fix Nix paths in Fish
    They were in the wrong order

--- config/fish/functions/prepend-nix-store-paths.fish
+++ config/fish/functions/prepend-nix-store-paths.fish
@@ -4,7 +4,9 @@ function prepend-nix-store-paths \
     --description "Move `/nix/store` paths to the front of `\$PATH`"
     set --local to_prepend
 
-    for i in (seq (count $PATH))
+    # Iterate in reverse order so that once we prepend the paths they'll be in
+    # the correct order.
+    for i in (seq (count $PATH) -1 1)
         switch $PATH[$i]
             case "/nix/store/*"
                 set --prepend to_prepend $i
@@ -12,6 +14,8 @@ function prepend-nix-store-paths \
     end
 
     if test (count $to_prepend) -gt 0
+        # Note: `$to_prepend` is an array so this gets expanded out to
+        # `$PATH[$to_prepend[1]] $PATH[$to_prepend[2]] ...`.
         fish_add_path --global --move --path $PATH[$to_prepend]
     end
 end
