18f20b485252562e8f9568160ec43e58883b01cb
Rebecca Turner <rbt@sent.as>
2024-05-29 10:37:48 -0700

    home-mangler: update pkgs

--- config/home-mangler/flake.nix
+++ config/home-mangler/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-mangler.url = "github:home-mangler/home-mangler";
   };
 
