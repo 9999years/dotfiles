785fbeaf95b661ca1dd41f19d74dbf6dd9745633
Rebecca Turner <rbt@sent.as>
2024-08-27 09:27:10 -0700

    nix: +repl-overlays

--- config/nix/repl.nix
+++ config/nix/repl.nix
@@ -1,9 +1,12 @@
 info: final: attrs: let
+  # Equivalent to nixpkgs `lib.optionalAttrs`.
   optionalAttrs = predicate: attrs:
     if predicate
     then attrs
     else {};
 
+  # If `attrs.${oldName}.${info.currentSystem}` exists, alias `${newName}` to
+  # it.
   collapseRenamed = oldName: newName:
     optionalAttrs (builtins.hasAttr oldName attrs
       && builtins.hasAttr info.currentSystem attrs.${oldName})
@@ -11,10 +14,17 @@ info: final: attrs: let
       ${newName} = attrs.${oldName}.${info.currentSystem};
     };
 
+  # Alias `attrs.${oldName}.${info.currentSystem} to `${newName}`.
   collapse = name: collapseRenamed name name;
 
-  collapseAll = builtins.foldl' (prev: name: prev // collapse name) {} (builtins.attrNames attrs);
+  # Alias all `attrs` keys with an `${info.currentSystem}` attribute.
+  collapseAll =
+    builtins.foldl'
+    (prev: name: prev // collapse name)
+    {}
+    (builtins.attrNames attrs);
 in
+  # Preserve the original bindings as `original`.
   (optionalAttrs (! attrs ? original)
     {
       original = attrs;
