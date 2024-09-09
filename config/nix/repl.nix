069e67f5238b04bdedadda3d541aa43c357a7e8b
Rebecca Turner <rbt@sent.as>
2024-09-09 16:18:47 -0700

    repl.nix: reformat with nixfmt-rfc-style

--- config/nix/repl.nix
+++ config/nix/repl.nix
@@ -1,16 +1,13 @@
-info: final: attrs: let
+info: final: attrs:
+let
   # Equivalent to nixpkgs `lib.optionalAttrs`.
-  optionalAttrs = predicate: attrs:
-    if predicate
-    then attrs
-    else {};
+  optionalAttrs = predicate: attrs: if predicate then attrs else { };
 
   # If `attrs.${oldName}.${info.currentSystem}` exists, alias `${newName}` to
   # it.
-  collapseRenamed = oldName: newName:
-    optionalAttrs (attrs ? oldName
-      && attrs.${oldName} ? ${info.currentSystem})
-    {
+  collapseRenamed =
+    oldName: newName:
+    optionalAttrs (attrs ? oldName && attrs.${oldName} ? ${info.currentSystem}) {
       ${newName} = attrs.${oldName}.${info.currentSystem};
     };
 
@@ -18,17 +15,12 @@ info: final: attrs: let
   collapse = name: collapseRenamed name name;
 
   # Alias all `attrs` keys with an `${info.currentSystem}` attribute.
-  collapseAll =
-    builtins.foldl'
-    (prev: name: prev // collapse name)
-    {}
-    (builtins.attrNames attrs);
+  collapseAll = builtins.foldl' (prev: name: prev // collapse name) { } (builtins.attrNames attrs);
 in
-  # Preserve the original bindings as `original`.
-  (optionalAttrs (! attrs ? original)
-    {
-      original = attrs;
-    })
-  // (collapseRenamed "packages" "pkgs")
-  // (collapseRenamed "legacyPackages" "pkgs")
-  // collapseAll
+# Preserve the original bindings as `original`.
+(optionalAttrs (!attrs ? original) {
+  original = attrs;
+})
+// (collapseRenamed "packages" "pkgs")
+// (collapseRenamed "legacyPackages" "pkgs")
+// collapseAll
