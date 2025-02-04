eeefe5290f0930b7a5782ed5668f5c39b3948518
Rebecca Turner <rbt@sent.as>
2025-02-04 10:13:02 -0800

    nix: add `getInputs`, `getNamedInput` helpers

--- config/nix/repl.nix
+++ config/nix/repl.nix
@@ -13,11 +13,48 @@ let
 
   # Alias `attrs.${oldName}.${info.currentSystem} to `${newName}`.
   collapse = name: collapseRenamed name name;
+
+  # Get `attrs.${name}` or `null` if it doesn't exist.
+  getAttrOrNull = name: attrs: if attrs ? ${name} then attrs.${name} else null;
+
+  # Get all input lists from `drv`.
+  # This does not include implicit inputs.
+  getInputs =
+    drv:
+    builtins.filter (drv: drv != null) (
+      builtins.concatLists [
+        (getAttrOrNull "buildInputs" drv)
+        (getAttrOrNull "nativeBuildInputs" drv)
+        (getAttrOrNull "propagatedBuildInputs" drv)
+        (getAttrOrNull "propagatedNativeBuildInputs" drv)
+      ]
+    );
+
+  # Get the input named `name` from the input lists of `drv`.
+  getNamedInput =
+    name: drv:
+    let
+      allInputs = builtins.filter (
+        input: getAttrOrNull "name" input == name || getAttrOrNull "pname" input == name
+      ) (getInputs drv);
+      length = builtins.length allInputs;
+    in
+    if length == 0 then
+      builtins.throw "No input found named ${name} from ${builtins.toString drv}"
+    else if length > 1 then
+      builtins.trace "Multiple (${length}}) inputs found named ${name} from ${builtins.toString drv}" (
+        builtins.elemAt allInputs 0
+      )
+    else
+      builtins.elemAt allInputs 0;
 in
 # Preserve the original bindings as `original`.
 (optionalAttrs (!attrs ? original) {
   original = attrs;
 })
+// {
+  inherit getInputs getNamedInput;
+}
 // (collapseRenamed "packages" "pkgs")
 // (collapseRenamed "legacyPackages" "pkgs")
 // collapse "checks"
