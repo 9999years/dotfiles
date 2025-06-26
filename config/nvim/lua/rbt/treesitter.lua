83f06232427d6835d5ef589119b3ea1f8419b0a7
Rebecca Turner <rbt@sent.as>
2025-06-26 10:08:34 -0700

    nvim: treesitter: reorganize
    reduce indentation

--- config/nvim/lua/rbt/treesitter.lua
+++ config/nvim/lua/rbt/treesitter.lua
@@ -1,103 +1,106 @@
 --- @type LazyPluginSpec
-return {
+local M = {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  dependencies = {
-    "nvim-treesitter/playground",
+}
 
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    "RRethy/nvim-treesitter-textsubjects",
+M.dependencies = {
+  "nvim-treesitter/playground",
 
-    -- Show "context" at the top of the window.
-    {
-      "nvim-treesitter/nvim-treesitter-context",
-      config = function()
-        -- TODO: This doesn't work with the treesitter 'module' system?
-        require("treesitter-context").setup {
-          -- Only show a couple lines.
-          max_lines = 3,
-          min_window_height = 30,
-          patterns = {
-            nix = {
-              "binding",
-            },
+  "nvim-treesitter/nvim-treesitter-textobjects",
+  "RRethy/nvim-treesitter-textsubjects",
+
+  -- Show "context" at the top of the window.
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    config = function()
+      -- TODO: This doesn't work with the treesitter 'module' system?
+      require("treesitter-context").setup {
+        -- Only show a couple lines.
+        max_lines = 3,
+        min_window_height = 30,
+        patterns = {
+          nix = {
+            "binding",
           },
-        }
-      end,
-    },
-
-    -- Matching
-    "vim-matchup",
-
-    require("rbt.fold"),
+        },
+      }
+    end,
   },
 
-  config = function()
-    -- See: https://github.com/nvim-treesitter/nvim-treesitter#available-modules
-    ---@diagnostic disable-next-line: missing-fields
-    require("nvim-treesitter.configs").setup {
-      matchup = {
+  -- Matching
+  "vim-matchup",
+
+  require("rbt.fold"),
+}
+
+function M.config()
+  -- See: https://github.com/nvim-treesitter/nvim-treesitter#available-modules
+  ---@diagnostic disable-next-line: missing-fields
+  require("nvim-treesitter.configs").setup {
+    matchup = {
+      enable = true,
+      disable = {},
+    },
+    ensure_installed = { "diff", "git_rebase" },
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+      disable = {
+        "markdown",
+        "gitcommit",
+        "make",
+      },
+    },
+    indent = {
+      -- maybe this sucks? (2025-06-20)
+      enable = false,
+      disable = {
+        "markdown",
+      },
+    },
+    textobjects = {
+      select = {
         enable = true,
-        disable = {},
-      },
-      ensure_installed = { "diff", "git_rebase" },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-        disable = {
-          "markdown",
-          "gitcommit",
-          "make",
-        },
-      },
-      indent = {
-        -- maybe this sucks? (2025-06-20)
-        enable = false,
-        disable = {
-          "markdown",
-        },
-      },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ["ia"] = "@parameter.inner",
-            ["aa"] = "@parameter.outer",
-            ["ic"] = "@class.inner",
-            ["ac"] = "@class.outer",
-            ["if"] = "@function.inner",
-            ["af"] = "@function.outer",
-            ["as"] = {
-              query = "@local.scope",
-              query_group = "locals",
-              desc = "Select language scope",
-            },
+        lookahead = true,
+        keymaps = {
+          ["ia"] = "@parameter.inner",
+          ["aa"] = "@parameter.outer",
+          ["ic"] = "@class.inner",
+          ["ac"] = "@class.outer",
+          ["if"] = "@function.inner",
+          ["af"] = "@function.outer",
+          ["as"] = {
+            query = "@local.scope",
+            query_group = "locals",
+            desc = "Select language scope",
           },
         },
       },
-      -- Visual mappings for selecting.
-      textsubjects = {
-        enable = true,
-        -- All only in visual mode!
-        prev_selection = ",",
-        keymaps = {
-          -- `v.` Select "the current thing".
-          -- `.`  Select "more".
-          ["."] = "textsubjects-smart",
-          -- Select the "outer" container.
-          [";"] = "textsubjects-container-outer",
-          -- Select the "inner" container.
-          ["i;"] = "textsubjects-container-inner",
-        },
+    },
+    -- Visual mappings for selecting.
+    textsubjects = {
+      enable = true,
+      -- All only in visual mode!
+      prev_selection = ",",
+      keymaps = {
+        -- `v.` Select "the current thing".
+        -- `.`  Select "more".
+        ["."] = "textsubjects-smart",
+        -- Select the "outer" container.
+        [";"] = "textsubjects-container-outer",
+        -- Select the "inner" container.
+        ["i;"] = "textsubjects-container-inner",
       },
-    }
+    },
+  }
 
-    -- https://neovim.discourse.group/t/git-diff-highlighting-are-not-working-anymore-in-gitcommit-filetype/3547/5
-    vim.cmd([[
-        highlight def link @text.diff.add DiffAdded
-        highlight def link @text.diff.delete DiffRemoved
-      ]])
-  end,
-}
+  -- https://neovim.discourse.group/t/git-diff-highlighting-are-not-working-anymore-in-gitcommit-filetype/3547/5
+  vim.cmd([[
+    highlight def link @text.diff.add DiffAdded
+    highlight def link @text.diff.delete DiffRemoved
+  ]])
+end
+
+return M
