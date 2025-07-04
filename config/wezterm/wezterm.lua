525dd70bf587634e891ebd54eb9b6a2d3a3974bb
Rebecca Turner <rbt@sent.as>
2025-07-04 16:55:47 -0700

    wezterm: init
    @evanrelf cultural victory

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.font = wezterm.font("PragmataProLiga Nerd Font")
+config.font_size = 16.0
+
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+
+return config
