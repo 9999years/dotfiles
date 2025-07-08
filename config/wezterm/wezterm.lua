49310e19956d43098b4c8012064dc238cadb1c91
Rebecca Turner <rbt@sent.as>
2025-07-08 14:24:23 -0700

    wezterm: reformat lua
    add stylua.toml and reformat

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -13,101 +13,53 @@ config.scrollback_lines = 100000
 
 -- Show which key table is active in the status area
 wezterm.on("update-right-status", function(window, _pane)
-	local name = window:active_key_table()
-	window:set_right_status(name or "")
+  local name = window:active_key_table()
+  window:set_right_status(name or "")
 end)
 
 config.leader = {
-	key = "b",
-	mods = "CTRL",
+  key = "b",
+  mods = "CTRL",
 }
 
 -- See: https://wezterm.org/config/keys.html
 -- See: https://wezterm.org/config/lua/keyassignment/
 config.keys = {
-	-- Split panes.
-	{
-		key = "|",
-		mods = "LEADER",
-		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = '"',
-		mods = "LEADER",
-		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
+  -- Split panes.
+  {
+    key = "|",
+    mods = "LEADER",
+    action = act.SplitHorizontal { domain = "CurrentPaneDomain" },
+  },
+  {
+    key = '"',
+    mods = "LEADER",
+    action = act.SplitVertical { domain = "CurrentPaneDomain" },
+  },
 
-	-- Zoom pane.
-	{
-		key = "z",
-		mods = "LEADER",
-		action = act.TogglePaneZoomState,
-	},
+  -- Zoom pane.
+  { key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
 
-	-- Switch pane.
-	{
-		key = "j",
-		mods = "LEADER",
-		action = act.ActivatePaneDirection("Down"),
-	},
-	{
-		key = "k",
-		mods = "LEADER",
-		action = act.ActivatePaneDirection("Up"),
-	},
-	{
-		key = "h",
-		mods = "LEADER",
-		action = act.ActivatePaneDirection("Left"),
-	},
-	{
-		key = "l",
-		mods = "LEADER",
-		action = act.ActivatePaneDirection("Right"),
-	},
+  -- Switch pane.
+  { key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+  { key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+  { key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+  { key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
 
-	-- Rotate panes.
-	{
-		key = "o",
-		mods = "LEADER|CTRL",
-		action = act.RotatePanes("Clockwise"),
-	},
-	{
-		key = "r",
-		mods = "LEADER|CTRL",
-		action = act.RotatePanes("CounterClockwise"),
-	},
+  -- Rotate panes.
+  { key = "o", mods = "LEADER|CTRL", action = act.RotatePanes("Clockwise") },
+  { key = "r", mods = "LEADER|CTRL", action = act.RotatePanes("CounterClockwise") },
 
-	-- Tabs.
-	{
-		key = "p",
-		mods = "LEADER",
-		action = act.ActivateTabRelative(-1),
-	},
-	{
-		key = "n",
-		mods = "LEADER",
-		action = act.ActivateTabRelative(1),
-	},
-	{
-		key = "c",
-		mods = "LEADER",
-		action = act.SpawnTab("CurrentPaneDomain"),
-	},
+  -- Tabs.
+  { key = "p", mods = "LEADER", action = act.ActivateTabRelative(-1) },
+  { key = "n", mods = "LEADER", action = act.ActivateTabRelative(1) },
+  { key = "c", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
 
-	-- Copy mode.
-	{
-		key = "[",
-		mods = "LEADER",
-		action = act.ActivateCopyMode,
-	},
+  -- Copy mode.
+  { key = "[", mods = "LEADER", action = act.ActivateCopyMode },
 
-	-- WezTerm-specific :)
-	{
-		key = "k",
-		mods = "CMD",
-		action = act.ActivateCommandPalette,
-	},
+  -- WezTerm-specific :)
+  { key = "k", mods = "CMD", action = act.ActivateCommandPalette },
 }
 
 return config
