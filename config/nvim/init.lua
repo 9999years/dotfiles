ff0c17cf1bb23ea3278a3bbaa0b9b921aa6365ac
Rebecca Turner <rbt@sent.as>
2025-07-21 22:34:40 -0400

    nvim: MergeConflicts: fix regex
    forgot to escape the pipes

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -380,7 +380,7 @@ batteries.cmd {
 batteries.cmd {
   "MergeConflicts",
   function()
-    vim.cmd([[/\M^\(<\{7}\||\{7}\|=\{7}\|>\{7}|+\{7}|%\{7}\)]])
+    vim.cmd([[/\M^\(<\{7}\||\{7}\|=\{7}\|>\{7}\|+\{7}\|%\{7}\)]])
   end,
   "Search for Git merge conflict markers",
 }
