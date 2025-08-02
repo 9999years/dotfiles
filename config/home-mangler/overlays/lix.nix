463cae1e6dbe06b320100289b90aaebc6977cef9
Rebecca Turner <rbt@sent.as>
2025-08-02 16:19:39 -0700

    home-mangler: lix overlay: actually use lix lol lmao
    this fixes software like npins

--- config/home-mangler/overlays/lix.nix
+++ config/home-mangler/overlays/lix.nix
@@ -1,4 +1,8 @@
 final: prev: {
+  lix = final.lixPackageSets.latest.lix;
+
+  nix = final.lix;
+
   nixVersions = final.lib.mapAttrs (
     _name: pkg: if builtins.isAttrs pkg then final.lix else pkg
   ) prev.nixVersions;
