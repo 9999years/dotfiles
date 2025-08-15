bb0c02b911b3c1d431bf3a18c2aba49f3c33edcd
Rebecca Turner <rbt@sent.as>
2025-08-15 11:43:56 -0700

    nvim: lsp: lazydev: monkeypatch to avoid deprecated call
    This avoids a call to the deprecated `client.notify` function.
    
    See: https://github.com/folke/lazydev.nvim/pull/106

--- config/nvim/lua/rbt/lsp.lua
+++ config/nvim/lua/rbt/lsp.lua
@@ -30,13 +30,36 @@ M.dependencies = {
   {
     "folke/lazydev.nvim",
     ft = "lua",
-    opts = {
-      library = {
-        -- See the configuration section for more details
-        -- Load luvit types when the `vim.uv` word is found
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-      },
-    },
+    config = function()
+      -- Monkeypatch in a PR to remove a call to the deprecated `client.notify`
+      -- function.
+      --
+      -- See: https://github.com/folke/lazydev.nvim/pull/106
+      local config = require("lazydev.config")
+      config.have_0_11 = vim.fn.has("nvim-0.11") == 1
+
+      local lsp = require("lazydev.lsp")
+      lsp.update = function(client)
+        lsp.assert(client)
+        if config.have_0_11 then
+          client:notify("workspace/didChangeConfiguration", {
+            settings = { Lua = {} },
+          })
+        else
+          client.notify("workspace/didChangeConfiguration", {
+            settings = { Lua = {} },
+          })
+        end
+      end
+
+      require("lazydev").setup {
+        library = {
+          -- See the configuration section for more details
+          -- Load luvit types when the `vim.uv` word is found
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        },
+      }
+    end,
   },
 }
 
