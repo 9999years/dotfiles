a74ece3fa9272f2570b170825fb6fc5da89fcb8d
Rebecca Turner <rbt@sent.as>
2025-08-21 21:22:15 -0700

    home-mangler: pythonEnv -> python-env

--- config/home-mangler/flake.nix
+++ config/home-mangler/flake.nix
@@ -113,7 +113,7 @@
             pkgs.pandoc
             pkgs.pre-commit
             pkgs.pyright
-            pkgs.rbt.pythonEnv
+            pkgs.rbt.python-env
             pkgs.rbt.pywatchman
             pkgs.rcm
             pkgs.ripgrep
