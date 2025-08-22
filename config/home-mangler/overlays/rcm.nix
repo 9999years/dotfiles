5e3092d18ed438f353e5325881318b79ffceee7e
Rebecca Turner <rbt@sent.as>
2025-08-21 23:10:30 -0700

    home-mangler: rcm: print when a new file is linked

--- config/home-mangler/overlays/rcm.nix
+++ config/home-mangler/overlays/rcm.nix
@@ -8,6 +8,14 @@ final: prev: {
         url = "https://github.com/thoughtbot/rcm/commit/20e8d6016087439af728ebb6b096f442152fd07b.patch";
         hash = "sha256-Jc7c63jEAwndzQ8OotOwtOvj4Rg2tj74FIxLE6AFfYE=";
       })
+
+      # Print when a new file is linked or copied
+      #
+      # See: https://github.com/thoughtbot/rcm/pull/310
+      (final.fetchpatch {
+        url = "https://github.com/thoughtbot/rcm/commit/ba9498541203f27a6a6ccef6fa19c232219b6111.patch";
+        hash = "sha256-Y2znU3vn+V5ToSkG4TxlVHBWYk4YWkmQS/sq5knLLjA=";
+      })
     ];
   });
 }
