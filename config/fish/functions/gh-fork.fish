14e27f58efbeb17a9e90e306b5b8e2330cff619a
Rebecca Turner <rbt@sent.as>
2025-09-09 11:37:33 -0700

    fish: gh-fork: ensure there's a jj repo to operate on

--- config/fish/functions/gh-fork.fish
+++ config/fish/functions/gh-fork.fish
@@ -1,5 +1,6 @@
 function gh-fork
     gh fork
+    jj init || true
     set -l new_trunk (jj config get 'revset-aliases."trunk()"' | sed 's/@origin/@upstream/')
     jj config set --repo 'revset-aliases."trunk()"' $new_trunk
     jj fetch --all-remotes
