334521273874b214b1039e31e0329669e7955eaa
Rebecca Turner <rbt@sent.as>
2025-10-23 00:14:01 -0700

    npingler: beets: rebase patches
    I also dropped a patch to make logging levels/format configurable here
    because it was too hard to rebase.

--- config/npingler/pkgs/beets-with-beetcamp.nix
+++ config/npingler/pkgs/beets-with-beetcamp.nix
@@ -1,5 +1,4 @@
 {
-  fetchFromGitHub,
   beets,
   chromaprint,
   rbt,
@@ -7,101 +6,48 @@
   lib,
 }:
 
-lib.warnIf (lib.versionAtLeast beets.version "2.3.2")
-  ''
-    beets is ${beets.version}, but our override bumps it to an unstable commit
-    after 2.3.1. Consider removing the `src` override.
-  ''
-
-  (beets.override {
-    src = fetchFromGitHub {
-      owner = "beetbox";
-      repo = "beets";
-      # `master` as of 2025-07-20.
-      # This is so patches apply cleanly.
-      # See: https://github.com/beetbox/beets/commit/0fec858a13854dd16cba5fcd6e698da148f4672d
-      rev = "0fec858a13854dd16cba5fcd6e698da148f4672d";
-      hash = "sha256-J2DDtXtq4o3lQvSGK4+SBY6JuhjLBRNIQZtiTGaYMDE=";
+(beets.override {
+  pluginOverrides = {
+    # See: https://github.com/NixOS/nixpkgs/pull/428173
+    chroma = {
+      wrapperBins = [
+        chromaprint
+      ];
     };
 
-    pluginOverrides = {
-      # See: https://github.com/NixOS/nixpkgs/pull/428173
-      chroma = {
-        wrapperBins = [
-          chromaprint
-        ];
-      };
-
-      beetcamp = {
-        enable = true;
-        propagatedBuildInputs = [ rbt.beetcamp ];
-      };
-
-      # These were added after 2.3.1 and aren't reflected in Nixpkgs.
-      replace.builtin = true;
-      musicbrainz.builtin = true;
-
-      # I added this one :)
-      detectmissing.builtin = true;
+    beetcamp = {
+      enable = true;
+      propagatedBuildInputs = [ rbt.beetcamp ];
     };
-  }).overridePythonAttrs
+
+    # These were added after 2.3.1 and aren't reflected in Nixpkgs.
+    replace.builtin = true;
+    musicbrainz.builtin = true;
+
+    # I added this one :)
+    detectmissing.builtin = true;
+  };
+}).overridePythonAttrs
   (prev: {
     patches = (prev.patches or [ ]) ++ [
-      # Don't crash if a Discogs release is deleted.
-      # See: https://github.com/beetbox/beets/pull/5893
-      (fetchpatch {
-        url = "https://github.com/beetbox/beets/commit/5a4066ada03d6a28891a9346d0ab85654fc32c4e.patch";
-        hash = "sha256-czftG20yE7FtWY5yBGQfpMGNCxftDxgoTPHyvG72fgM=";
-      })
-
-      # chroma: set a default timeout of 10 seconds
-      #
-      # See: https://github.com/beetbox/beets/pull/5898
-      (fetchpatch {
-        url = "https://github.com/beetbox/beets/commit/8ce0ad71d7bf10f50f17c2966f9e439cafae73d0.patch";
-        hash = "sha256-spdvev2eYeUInf8m5VplrbAS7g5LzJrlOcHV6KHj7lU=";
-      })
-
-      # logging: make levels/format configurable
+      # NB: Dropped a patch to make logging levels/format configurable here
+      # because it was too hard to rebase.
       #
       # See: https://github.com/beetbox/beets/pull/5897
-      (fetchpatch {
-        # logging: use module names as logger names
-        url = "https://github.com/beetbox/beets/commit/2fc940852213a6572f87fd5be47e80562d7bb09a.patch";
-        hash = "sha256-yT9gRv98z6GCJTnlWHjtBUPjFViRpuOD0b8v7Cc11sU=";
-      })
-      (fetchpatch {
-        # logging: use beets logger in more places
-        url = "https://github.com/beetbox/beets/commit/a2963fa0c16be13ea4865cf0235c661d3c365d89.patch";
-        hash = "sha256-J6tMuZUXMr/RQTgt33Q0bqiRTymxRDlzIsA8IEhQVBk=";
-      })
-      (fetchpatch {
-        # logging: make log levels/format configurable
-        url = "https://github.com/beetbox/beets/commit/7e93ce71c93c7abef4e9e98889a37741d0cb8dca.patch";
-        hash = "sha256-n3X8WoujIt7sN8NuRwKp49lJi5APr9+GL5vlX24OwcQ=";
-      })
-
-      # fromfilename: Don't crash if title is missing
-      #
-      # See: https://github.com/beetbox/beets/pull/5907
-      (fetchpatch {
-        url = "https://github.com/beetbox/beets/commit/37728dcc3012ea56bc91300e49924d7c67b838bb.patch";
-        hash = "sha256-FK9TYBZk+6QYH1ZHdlzWDO3J6kuZv4nYiIEph3JOtN0=";
-      })
 
       # spotify: Don't crash if a network request fails
       #
       # See: https://github.com/beetbox/beets/pull/5910
       (fetchpatch {
-        url = "https://github.com/beetbox/beets/commit/08b239f350db4a5e47a0625025a70585420ddf98.patch";
-        hash = "sha256-w3aIAvy7rj3Vmif5FUI0L0guMWjghsmTYkDNJ6Xac3w=";
+        url = "https://github.com/beetbox/beets/commit/bf927a5d36d47791f5a0162622c3683aa0600060.patch";
+        hash = "sha256-kNoY4QpaKwfqDGLE2KFqkCi7dqzfpEAj5LqTvAYABtw=";
       })
 
       # detectmissing: init
       #
       # See: https://github.com/beetbox/beets/pull/5912
       (fetchpatch {
-        url = "https://github.com/beetbox/beets/commit/e5e3f30e80debdcdec2e282a0ed43f7c08de256a.patch";
+        url = "https://github.com/beetbox/beets/commit/288d5e7dc701aff39ea98dfeefce2a1fbf384842.patch";
         hash = "sha256-m6578KqQZoTMsLlOTgKOYQoRW0XQ96Gy2JmI7Ufal5g=";
       })
 
@@ -109,8 +55,8 @@ lib.warnIf (lib.versionAtLeast beets.version "2.3.2")
       #
       # See: https://github.com/beetbox/beets/pull/5927
       (fetchpatch {
-        url = "https://github.com/beetbox/beets/commit/e352c215aa2e265a1b20429c690818e8b62adffa.patch";
-        hash = "sha256-NXIWFbzEe701Ugi9EHm/sFolTbCZ4kELTVMsViXwiNI=";
+        url = "https://github.com/beetbox/beets/commit/f0b535488996136f648c91cbc7eed7d810d377cf.patch";
+        hash = "sha256-UNc51I6UH7L+w4Y5S9vV0logmRroCMFqpXn9NxVV1iM=";
       })
     ];
 
