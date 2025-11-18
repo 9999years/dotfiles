ff2c3da0fc12ab7bb6afb2ba7c71bbd578610e8c
Rebecca Turner <rbt@sent.as>
2025-11-18 15:25:31 -0800

    nvim: enable the horrible terraform language server when relevant

--- config/nvim/lua/rbt/lsp.lua
+++ config/nvim/lua/rbt/lsp.lua
@@ -450,6 +450,7 @@ function M.config()
   vim.lsp.enable("racket_langserver")
   vim.lsp.enable("rust_analyzer")
   vim.lsp.enable("taplo") -- TOML schemas: https://taplo.tamasfe.dev/
+  vim.lsp.enable("terraformls")
   vim.lsp.enable("texlab") -- LaTeX
   vim.lsp.enable("ts_ls")
   vim.lsp.enable("yamlls")
