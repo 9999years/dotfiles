c5282924a0db950f8728ad76fe417f7be76ce159
Rebecca Turner <rbt@sent.as>
2026-01-21 17:49:28 -0800

    fish: gh-fork: ignore `jj init` errors
    Usually warnings about already having a `jj` repo

--- config/fish/functions/gh-fork.fish
+++ config/fish/functions/gh-fork.fish
@@ -1,6 +1,6 @@
 function gh-fork
     gh fork
-    jj init || true
+    jj init 2>/dev/null || true
     set -l new_trunk (jj config get 'revset-aliases."trunk()"' | sed 's/@origin/@upstream/')
     jj config set --repo 'revset-aliases."trunk()"' $new_trunk
     jj fetch --all-remotes
