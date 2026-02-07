5909caa24deac8457e9ecf26b19855b6363888e4
Rebecca Turner <rbt@sent.as>
2026-02-07 15:49:07 -0800

    npingler: beets-with-beetcamp: use `callPackage`
    actually with whitespace ignored it's a very small diff

--- config/npingler/pkgs/beets-with-beetcamp.nix
+++ config/npingler/pkgs/beets-with-beetcamp.nix
@@ -4,66 +4,75 @@
   beets-with-beetcamp,
 }:
 
-(python3.pkgs.toPythonApplication (
-  python3.pkgs.beets.override {
-    # Needed until we have https://github.com/NixOS/nixpkgs/pull/477084
-    # Ughhhhh.
-    beets = beets-with-beetcamp;
+python3.pkgs.callPackage (
+  {
+    toPythonApplication,
+    beets,
+    beetcamp,
+    beets-copyartifacts3,
+  }:
 
-    pluginOverrides = {
-      beetcamp = {
-        enable = true;
-        propagatedBuildInputs = [ python3.pkgs.beetcamp ];
+  (toPythonApplication (
+    beets.override {
+      # Needed until we have https://github.com/NixOS/nixpkgs/pull/477084
+      # Ughhhhh.
+      beets = beets-with-beetcamp;
+
+      pluginOverrides = {
+        beetcamp = {
+          enable = true;
+          propagatedBuildInputs = [ beetcamp ];
+        };
+
+        copyartifacts = {
+          enable = true;
+          propagatedBuildInputs = [ beets-copyartifacts3 ];
+        };
+
+        # I added this one :)
+        #
+        # Can't put this in `pluginOverrides`.
+        # See: https://github.com/NixOS/nixpkgs/pull/471166
+        detectmissing = {
+          builtin = true;
+        };
       };
 
-      copyartifacts = {
-        enable = true;
-        propagatedBuildInputs = [ python3.pkgs.beets-copyartifacts3 ];
-      };
+      extraPatches = [
+        # NB: Dropped a patch to make logging levels/format configurable here
+        # because it was too hard to rebase.
+        #
+        # See: https://github.com/beetbox/beets/pull/5897
 
-      # I added this one :)
-      #
-      # Can't put this in `pluginOverrides`.
+        # spotify: Don't crash if a network request fails
+        #
+        # See: https://github.com/beetbox/beets/pull/5910
+        (fetchpatch {
+          url = "https://github.com/beetbox/beets/commit/bf927a5d36d47791f5a0162622c3683aa0600060.patch";
+          hash = "sha256-kNoY4QpaKwfqDGLE2KFqkCi7dqzfpEAj5LqTvAYABtw=";
+        })
+
+        # detectmissing: init
+        #
+        # See: https://github.com/beetbox/beets/pull/5912
+        (fetchpatch {
+          url = "https://github.com/beetbox/beets/commit/288d5e7dc701aff39ea98dfeefce2a1fbf384842.patch";
+          hash = "sha256-m6578KqQZoTMsLlOTgKOYQoRW0XQ96Gy2JmI7Ufal5g=";
+        })
+
+        # fish: complete files in more places
+        #
+        # See: https://github.com/beetbox/beets/pull/5927
+        (fetchpatch {
+          url = "https://github.com/beetbox/beets/commit/f0b535488996136f648c91cbc7eed7d810d377cf.patch";
+          hash = "sha256-UNc51I6UH7L+w4Y5S9vV0logmRroCMFqpXn9NxVV1iM=";
+        })
+      ];
+    }
+  )).overridePythonAttrs
+    (prev: {
+      # lol lmao
       # See: https://github.com/NixOS/nixpkgs/pull/471166
-      detectmissing = {
-        builtin = true;
-      };
-    };
-
-    extraPatches = [
-      # NB: Dropped a patch to make logging levels/format configurable here
-      # because it was too hard to rebase.
-      #
-      # See: https://github.com/beetbox/beets/pull/5897
-
-      # spotify: Don't crash if a network request fails
-      #
-      # See: https://github.com/beetbox/beets/pull/5910
-      (fetchpatch {
-        url = "https://github.com/beetbox/beets/commit/bf927a5d36d47791f5a0162622c3683aa0600060.patch";
-        hash = "sha256-kNoY4QpaKwfqDGLE2KFqkCi7dqzfpEAj5LqTvAYABtw=";
-      })
-
-      # detectmissing: init
-      #
-      # See: https://github.com/beetbox/beets/pull/5912
-      (fetchpatch {
-        url = "https://github.com/beetbox/beets/commit/288d5e7dc701aff39ea98dfeefce2a1fbf384842.patch";
-        hash = "sha256-m6578KqQZoTMsLlOTgKOYQoRW0XQ96Gy2JmI7Ufal5g=";
-      })
-
-      # fish: complete files in more places
-      #
-      # See: https://github.com/beetbox/beets/pull/5927
-      (fetchpatch {
-        url = "https://github.com/beetbox/beets/commit/f0b535488996136f648c91cbc7eed7d810d377cf.patch";
-        hash = "sha256-UNc51I6UH7L+w4Y5S9vV0logmRroCMFqpXn9NxVV1iM=";
-      })
-    ];
-  }
-)).overridePythonAttrs
-  (prev: {
-    # lol lmao
-    # See: https://github.com/NixOS/nixpkgs/pull/471166
-    doCheck = false;
-  })
+      doCheck = false;
+    })
+) { }
