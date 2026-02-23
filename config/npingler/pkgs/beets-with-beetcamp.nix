43868f3bd38f4ec10c4c3b20ffd210f447e68159
Rebecca Turner <rbt@sent.as>
2026-02-23 11:41:15 -0800

    npingler: beets: fix build
    These patches were merged upstream, neat!

--- config/npingler/pkgs/beets-with-beetcamp.nix
+++ config/npingler/pkgs/beets-with-beetcamp.nix
@@ -44,28 +44,12 @@ python3.pkgs.callPackage (
         #
         # See: https://github.com/beetbox/beets/pull/5897
 
-        # spotify: Don't crash if a network request fails
-        #
-        # See: https://github.com/beetbox/beets/pull/5910
-        (fetchpatch {
-          url = "https://github.com/beetbox/beets/commit/bf927a5d36d47791f5a0162622c3683aa0600060.patch";
-          hash = "sha256-kNoY4QpaKwfqDGLE2KFqkCi7dqzfpEAj5LqTvAYABtw=";
-        })
-
         # detectmissing: init
         #
         # See: https://github.com/beetbox/beets/pull/5912
         (fetchpatch {
           url = "https://github.com/beetbox/beets/commit/288d5e7dc701aff39ea98dfeefce2a1fbf384842.patch";
           hash = "sha256-m6578KqQZoTMsLlOTgKOYQoRW0XQ96Gy2JmI7Ufal5g=";
-        })
-
-        # fish: complete files in more places
-        #
-        # See: https://github.com/beetbox/beets/pull/5927
-        (fetchpatch {
-          url = "https://github.com/beetbox/beets/commit/f0b535488996136f648c91cbc7eed7d810d377cf.patch";
-          hash = "sha256-UNc51I6UH7L+w4Y5S9vV0logmRroCMFqpXn9NxVV1iM=";
         })
       ];
     }
