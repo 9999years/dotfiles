105801003916fd0160c5c056beb91bcb711515f5
Rebecca Turner <rbt@sent.as>
2026-02-23 11:41:15 -0800

    npingler: use claude-mergetool from nixpkgs

--- config/npingler/default.nix
+++ config/npingler/default.nix
@@ -51,7 +51,7 @@ let
         pkgs.cargo-edit # `cargo upgrade`
         pkgs.cargo-nextest
         pkgs.cargo-watch
-        pkgs.rbt.claude-mergetool
+        pkgs.claude-mergetool
         pkgs.delta # `git-delta`
         pkgs.dig
         pkgs.direnv
